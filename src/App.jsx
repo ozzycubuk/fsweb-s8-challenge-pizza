a2e19aca97f36af35a44789156a9613e41a39fb3
ozzycubuk <oguzhancubuk@hotmail.com>
2025-07-09 01:41:21 +0300

    iterasyon 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
 import Success from "./pages/Success";
 
 function App() {
+  const [siparisData, setSiparisData] = useState(null);
+  const [hataMesaji, setHataMesaji] = useState("");
+
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/order" component={Order} />
-        <Route path="/success" component={Success} />
+        
+        <Route
+          path="/order"
+          render={(props) => (
+            <Order
+              {...props}
+              setSiparisData={setSiparisData}
+              setHataMesaji={setHataMesaji}
+            />
+          )}
+        />
+        
+        <Route
+          path="/success"
+          render={(props) => (
+            <Success
+              {...props}
+              siparisData={siparisData}
+              hataMesaji={hataMesaji}
+              setHataMesaji={setHataMesaji}
+            />
+          )}
+        />
       </Switch>
     </Router>
   );
